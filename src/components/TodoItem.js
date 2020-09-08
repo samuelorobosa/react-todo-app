c1ba6e64a3f2ce3bf41fe8ab7c965863308a88c3
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-09-08 15:41:58 +0100

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -14,12 +14,19 @@ class TodoItem extends React.Component {
 	render() {
 		return (
 			<div className="container" style={this.crossOut()}>
-			<input type="checkbox" onChange = {this.props.finishTask.bind
-				(this, this.props.todo.id) />
-			<div> {this.props.todo.message} </div>
-			<div> {this.props.todo.date} </div>
-			<div onClick = {this.props.deleteTodo.bind(
-				this, this.props.todo.id)} className="delete"> &#9988; </div>
+			
+			<input type="checkbox" 
+			onChange = {this.props.finishTask.bind (this, this.props.todo.id) 
+				    />
+			<div> 
+				    {this.props.todo.message} 
+		</div>
+			<div> 
+				{this.props.todo.date} 
+		</div>
+			<div onClick = {this.props.deleteTodo.bind(this, this.props.todo.id)} className="delete"> 
+				&#9988; 
+				</div>
 
 			</div>
 		)
